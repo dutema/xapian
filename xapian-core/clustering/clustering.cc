08dd1a99d1f89326777dde6ffd08884055a6b370
George Daniel MITRA <george.mitra@cti.pub.ro>
2014-06-11 17:21:10 +0300

    stubs

---
+++ xapian-core/clustering/clustering.cc
@@ -0,0 +1,4 @@
+#include <config.h>
+#include "xapian/clustering.h"
+
+gogu
---
+++ xapian-core/include/xapian/clustering.h
@@ -0,0 +1,10 @@
+#ifndef XAPIAN_INCLUDED_CLUSTER_H
+#define XAPIAN_INCLUDED_CLUSTER_H
+
+namespace Xapian {
+
+class Document;
+
+}
+
+#endif // XAPIAN_INCLUDED_CLUSTER_H
